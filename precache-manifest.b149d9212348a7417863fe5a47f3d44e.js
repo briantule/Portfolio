98178ded4f3475729b00cc10f71d2c5e7ef3a045
Brian Le <briantule3@gmail.com>
2020-10-25 19:35:29 -0400

    Updates

---
+++ precache-manifest.b149d9212348a7417863fe5a47f3d44e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a7a452ffcdcc0d69ffb294ea4aadc62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "987c65d968baa658396b",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "781334d3b3bbb715b46b",
+    "url": "/static/css/main.bd3de980.chunk.css"
+  },
+  {
+    "revision": "987c65d968baa658396b",
+    "url": "/static/js/2.a5bb21b3.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.a5bb21b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "781334d3b3bbb715b46b",
+    "url": "/static/js/main.f0f43336.chunk.js"
+  },
+  {
+    "revision": "2b5439e16cf819a1e33c",
+    "url": "/static/js/runtime-main.a1041db0.js"
+  }
+]);
