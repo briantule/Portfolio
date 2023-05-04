e9a1f9ed02df0cd95c10f47918998d4cdc48dd13
Brian Le <briantule3@gmail.com>
2023-05-04 14:28:29 -0400

    Update site

--- src/App.js
+++ src/App.js
@@ -1,38 +1,15 @@
 import React from 'react';
-import './App.css';
-import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
-import Main from './components/main';
-import { Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="demo-big-content">
-      <Layout>
-        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">MyPortfolio</Link>} scroll>
-          <Navigation>
-            <Link to="/resume">Resume</Link>
-            <Link to="/aboutme">About Me</Link>
-            <Link to="/projects">Projects</Link>
-            <Link to="/contact">Contact</Link>
-            <Link to="/path">Path</Link>
-          </Navigation>
-        </Header>
-        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">MyPortfolio</Link>}>
-          <Navigation>
-            <Link to="/resume">Resume</Link>
-            <Link to="/aboutme">About Me</Link>
-            <Link to="/projects">Projects</Link>
-            <Link to="/contact">Contact</Link>
-            <Link to="/path">Path</Link>
-          </Navigation>
-        </Drawer>
-        <Content>
-          <div className="page-content" />
-          <Main></Main>
-        </Content>
-      </Layout>
-    </div>
-  );
-}
+// import components
+import Header from './components/Header';
+import Hero from './components/Hero';
+
+const App = () => {
+  return <div>
+    <Header />
+    <Hero />
+    <div style={{ height: '2000px' }}></div>
+  </div>;
+};
 
 export default App;
