d7d8b3f1e959c8c9214fb6e8c5d4692ab1329d32
Brian Le <briantule3@gmail.com>
2023-05-07 14:45:35 -0400

    footer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,32 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import '../styles/Navbar.css'
+import React, {useEffect, useState} from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import '../styles/Navbar.css';
+import ReorderIcon from '@material-ui/icons/Reorder';
 
 function Navbar() {
-  return (
-    <div className="navbar">
-        <div className="toggleButton"> 
-            <button></button>
+    const [expandNavbar, setExpandNavbar] = useState(false);
+    const location = useLocation();
+
+    useEffect(() => {
+        setExpandNavbar(false)
+    }, [location]);
+    
+    return (
+        <div className="navbar" id={expandNavbar ? "open" : "close"}>
+            <div className="toggleButton">
+                <button onClick={() => {
+                    setExpandNavbar((prev) => !prev);
+                }}>
+                    <ReorderIcon />
+                </button>
+            </div>
+            <div className="links">
+                <Link to="/"> Home </Link>
+                <Link to="/projects"> Projects </Link>
+                <Link to="/experience"> Experience </Link>
+            </div>
         </div>
-        <div className="links">
-            <Link to="/"> Home </Link>
-            <Link to="/projects"> Projects </Link>
-            <Link to="/experience"> Experience </Link>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
