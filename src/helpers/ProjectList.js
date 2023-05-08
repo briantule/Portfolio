a60da4f0f14907534e91690876dbebbbd2495de7
Brian Le <briantule3@gmail.com>
2023-05-07 22:13:22 -0400

    Project disp

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,13 +1,45 @@
+import ProjPortfolio from '../assets/proj-portfolio.jpeg';
+import ProjCyclops from '../assets/proj-cyclops.jpeg';
 import ProjvSafe from '../assets/proj-vSafe.jpeg';
+import ProjSpecimen from '../assets/proj-specimen.jpeg';
 import ProjBomber from '../assets/proj-bomber.jpeg';
+import ProjStock from '../assets/proj-stock.jpeg';
+import ProjLED from '../assets/proj-led.jpeg';
 
 export const ProjectList = [
     {
+        name: "Peronal Portfolio",
+        image: ProjPortfolio,
+        skills: "JavaScript, HTML, CSS, ReactJS",
+    },
+    {
+        name: "Cyclops",
+        image: ProjCyclops,
+        skills: "Python, Shell",
+    },
+    {
         name: "vSafe",
         image: ProjvSafe,
+        skills: "Java, JavaScript, HTML, CSS",
     },
     {
         name: "Bomberman",
         image: ProjBomber,
+        skills: "Java",
     },
+    {
+        name: "Specimen-Game",
+        image: ProjSpecimen,
+        skills: "C#, Unity",
+    },
+    {
+        name: "Stock-Predict",
+        image: ProjStock,
+        skills: "Python",
+    },
+    {
+        name: "LED-Guage",
+        image: ProjLED,
+        skills: "C, Matlab",
+    }
 ];
