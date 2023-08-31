8b35baa351a25b0c36646635d1b2d698be61a31d
Brian Le <briantule3@gmail.com>
2023-08-31 04:02:22 -0400

    Upload resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { Document, Page, pdfjs } from 'react-pdf';
 import resume from '../assets/Resume - Brian Tu Le.pdf'
+import "react-pdf/dist/esm/Page/TextLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
 function Resume() {
   return (
     <div className="resume">
-      <Document 
-        file={resume}
-        onLoadError={console.error}
-        style={{width: 'auto', height: 'auto'}}
-      >
-        <Page pageIndex={0} />
-      </Document>
+      <center>
+        <Document 
+          file={resume}
+          onLoadError={console.error}
+          style={{width: 'auto', height: 'auto'}}
+         >
+            <Page pageIndex={0} enderTextLayer={false}/>
+        
+        </Document>
+      </center>
+      
     </div>
   );
 }
